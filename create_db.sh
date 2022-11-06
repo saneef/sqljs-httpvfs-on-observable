7c95f1e30bfd1c1b7a9eb260125ccebeceb795bd
Saneef Ansari <hello@saneef.com>
2022-11-06 20:07:00 +0530

    Change chunked file name

--- create_db.sh
+++ create_db.sh
@@ -4,14 +4,15 @@ set -eu
 
 indb="$1"
 outdir="$2"
+urlPrefix="db-sqlite3-"
 
 # for chunked mode, we need to know the database size in bytes beforehand
 bytes="$(stat -f "%z" "$indb")"
 # set chunk size to 10MiB (needs to be a multiple of the `pragma page_size`!)
 serverChunkSize=$((10 * 1024 * 1024))
 suffixLength=3
-rm -f "$outdir/db.sqlite3"*
-split -d -b $serverChunkSize -a $suffixLength "$indb" "$outdir/db.sqlite3."
+rm -f "$outdir/*"
+split -d -b $serverChunkSize -a $suffixLength "$indb" "$outdir/$urlPrefix"
 
 # set request chunk size to match page size
 requestChunkSize="$(sqlite3 "$indb" 'pragma page_size')"
@@ -23,7 +24,7 @@ echo '
     "requestChunkSize": '$requestChunkSize',
     "databaseLengthBytes": '$bytes',
     "serverChunkSize": '$serverChunkSize',
-    "urlPrefix": "db.sqlite3.",
+    "urlPrefix": "'$urlPrefix'",
     "suffixLength": '$suffixLength'
 }
 ' > "$outdir/config.json"
